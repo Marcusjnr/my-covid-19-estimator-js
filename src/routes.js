ca41deb7127a503053f57f7a55b7f40665ee3d7d
marcusjnr <okochaebube@gmail.com>
2020-04-12 14:04:34 +0100

    added xml link

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const fs = require('fs');
+const js2xmlparser = require('js2xmlparser');
 const covid19ImpactEstimator = require('./estimator');
 
 const logs = {
@@ -45,24 +46,24 @@ router.post('/api/v1/on-covid-19/json', async (req, res) => {
 });
 
 
-// app.get('/api/v1/on-covid-19/xml', async (req, res) => {
-//   const start = new Date();
-//   const estimateXml = builder.buildObject(covid19ImpactEstimator(req.body));
-//   res.send(estimateXml);
-//   req.on('close', (() => {
-//     const stop = new Date();
-//     logs.log_data.push({
-//       request: 'GET',
-//       url: '/api/v1/on-covid-19/xml',
-//       status: 200,
-//       time: stop - start
-//     });
-//     const json = JSON.stringify(logs);
-//     fs.writeFile('./src/log.json', json, 'utf8', (() => {
-//
-//     }));
-//   }));
-// });
+router.post('/api/v1/on-covid-19/xml', async (req, res) => {
+  const start = new Date();
+  const estimateXml = js2xmlparser.parse('root', covid19ImpactEstimator(req.body));
+  res.send(estimateXml);
+  req.on('close', (() => {
+    const stop = new Date();
+    logs.log_data.push({
+      request: 'GET',
+      url: '/api/v1/on-covid-19/xml',
+      status: 200,
+      time: stop - start
+    });
+    const json = JSON.stringify(logs);
+    fs.writeFile('./src/log.json', json, 'utf8', (() => {
+
+    }));
+  }));
+});
 
 // app.get('/api/v1/on-covid-19/logs', async (req, res) => {
 //
